1d26933f03539e3f94cb3a5c6efb5023708485aa
Humberto <humberto.moraes.junior@gmail.com>
2021-04-15 21:00:18 -0300

    Correçao no path do projeto - server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('index.html', {root: './dist/TESTE-TOPI-FRONTEND/'}));
+app.use(express.static('index.html', {root: './dist/Teste-Topi-FrontEnd/'}));
 
-app.get('/*', (req, res) => res.sendFile('index.html', {root: './dist/TESTE-TOPI-FRONTEND/'}));
+app.get('/*', (req, res) => res.sendFile('index.html', {root: './dist/Teste-Topi-FrontEnd/'}));
 
 app.listen(process.env.PORT || 8080);
