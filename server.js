8992ec2db72595b5da083164abe69b968b704e5c
Humberto <humberto.moraes.junior@gmail.com>
2021-04-15 21:23:18 -0300

    Alterando variaveis de ambiente no server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
-
+const nomeApp = process.env.npm_package_name;
 
 const app = express();
 
-app.use(express.static('index.html', {root: './dist/teste-topi-frontend-humberto/'}));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+//   'index.html', {root: './Teste-Topi-FrontEnd/'}));
 
-app.get('/*', (req, res) => res.sendFile('index.html', {root: './dist/teste-topi-frontend-humberto/'}));
+app.get('/*', (req, res) => res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`)));
+//   'index.html', {root: './Teste-Topi-FrontEnd/'}));
 
 app.listen(process.env.PORT || 8080);
